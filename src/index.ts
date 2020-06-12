acbe8934599f09979d06335c8a9023a8223a5c91
Brais Piñeiro <brapifra@gmail.com>
2020-06-12 23:14:36 +0100

    Add basic data types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,50 @@
-export default async function (event, context) {
+import { APIGatewayProxyHandler } from 'aws-lambda';
+
+interface EventBody {
+  passengers: PassengerDTO[];
+  aircraft: string;
+  itinerary: Itinerary;
+}
+
+interface PassengerDTO {
+  id: string;
+  dateOfBirth: string;
+  groupId: string;
+  itinerary: Itinerary[];
+}
+
+interface Itinerary {
+  departure: string;
+  arrival: string;
+}
+
+const handler: APIGatewayProxyHandler = async (event, context) => {
+  const body = parseEventBody(event.body || '{}');
+
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello world!',
-      input: event,
-    }),
+    body: JSON.stringify(body),
+  };
+};
+
+export default handler;
+
+type ParsedEventBody = Omit<EventBody, 'passengers'> & {
+  passengers: Passenger[];
+};
+
+type Passenger = Omit<PassengerDTO, 'dateOfBirth'> & {
+  dateOfBirth: Date;
+};
+
+function parseEventBody(rawBody: string): ParsedEventBody {
+  const body: EventBody = JSON.parse(rawBody);
+
+  return {
+    ...body,
+    passengers: body.passengers.map((passenger) => ({
+      ...passenger,
+      dateOfBirth: new Date(passenger.dateOfBirth),
+    })),
   };
 }
