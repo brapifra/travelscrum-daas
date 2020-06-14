0a5510a20483d9a840d0a60a49814a33ad75a46d
Samuel Andreo <samuel.andreo.vilaplana@ibm.com>
2020-06-14 21:14:31 +0100

    Add data to playground

--- src/apiRunner/main.js
+++ src/apiRunner/main.js
@@ -1,108 +1,105 @@
-const allocate = async target => {
-    const body = {
-        "passengers": [
-            {
-                "id": "father",
-                "groupId": "family",
-                "itinerary": [
-                    {
-                        "departure": "ES",
-                        "arrival": "GB"
-                    }
-                ]
-            },
-            {
-                "id": "son",
-                "groupId": "family",
-                "itinerary": [
-                    {
-                        "departure": "ES",
-                        "arrival": "GB"
-                    }
-                ]
-            },
-            {
-                "id": "businesswoman",
-                "itinerary": [
-                    {
-                        "departure": "SK",
-                        "arrival": "ES"
-                    },
-                    {
-                        "departure": "ES",
-                        "arrival": "GB"
-                    }
-                ]
-            },		{
-                "id": "businessman",
-                "itinerary": [
-                    {
-                        "departure": "CH",
-                        "arrival": "ES"
-                    },
-                    {
-                        "departure": "ES",
-                        "arrival": "GB"
-                    }
-                ]
-            }
+const allocate = async (target) => {
+  const body = {
+    passengers: [
+      {
+        id: 'PAS-1',
+        groupId: 'BOOKING-1',
+        age: '45',
+        itinerary: [
+          {
+            departure: 'ES',
+            arrival: 'GB',
+          },
         ],
-        "aircraft": "333",
-        "itinerary": {
-            "departure": "ES",
-            "arrival": "GB"
-        }
-    }
-    const endpoint = 'https://d606brmdeh.execute-api.eu-west-2.amazonaws.com/dev/seat/allocation';
-    target.innerHTML="Allocating...";
-    const response = await fetch(endpoint, {
-        method: 'post',
-        body: JSON.stringify(body),
-        mode: 'cors'
-    });
-    const data = await response.json();
-    console.log(data)
+      },
+      {
+        id: 'PAS-2',
+        groupId: 'BOOKING-1',
+        age: '8',
+        itinerary: [
+          {
+            departure: 'ES',
+            arrival: 'GB',
+          },
+        ],
+      },
+      {
+        id: 'PAS-3',
+        age: '34',
+        specialServiceRequest: 'respiratory-condition',
+        itinerary: [
+          {
+            departure: 'SK',
+            arrival: 'ES',
+          },
+          {
+            departure: 'ES',
+            arrival: 'GB',
+          },
+        ],
+      },
+    ],
+    aircraft: 'Boeing 787-8',
+    itinerary: {
+      departure: 'ES',
+      arrival: 'GB',
+    },
+  };
+  const endpoint =
+    'https://d606brmdeh.execute-api.eu-west-2.amazonaws.com/dev/seat/allocation';
+  target.innerHTML = 'Allocating...';
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify(body),
+    mode: 'cors',
+  });
+  const data = await response.json();
+  console.log(data);
 
-    target.innerHTML="Allocate!";
-    const allocationResponse = document.getElementById('allocationResponse');
+  target.innerHTML = 'Allocate!';
+  const allocationResponse = document.getElementById('allocationResponse');
 
-    allocationResponse.parentElement.classList.remove('prettyprinted');
-    allocationResponse.append(JSON.stringify(data, null, 2));
-    PR.prettyPrint();
-}
+  allocationResponse.parentElement.classList.remove('prettyprinted');
+  allocationResponse.append(JSON.stringify(data, null, 2));
+  PR.prettyPrint();
+};
 
-const getPassengerSeat = async target => {
-    const endpoint = 'https://d606brmdeh.execute-api.eu-west-2.amazonaws.com/dev/passenger/1/seat/1'
-    target.innerHTML="Allocating...";
-    const response = await fetch(endpoint, {
-        method: 'get',
-        mode: 'cors'
-    });
-    const data = await response.json();
-    console.log(data)
+const getPassengerSeat = async (target) => {
+  const endpoint =
+    'https://d606brmdeh.execute-api.eu-west-2.amazonaws.com/dev/passenger/1/seat/1';
+  target.innerHTML = 'Allocating...';
+  const response = await fetch(endpoint, {
+    method: 'get',
+    mode: 'cors',
+  });
+  const data = await response.json();
+  console.log(data);
 
-    target.innerHTML="Allocate!";
-    const allocationResponse = document.getElementById('getPassengerSeatResponse');
+  target.innerHTML = 'Allocate!';
+  const allocationResponse = document.getElementById(
+    'getPassengerSeatResponse'
+  );
 
-    allocationResponse.parentElement.classList.remove('prettyprinted');
-    allocationResponse.append(JSON.stringify(data, null, 2));
-    PR.prettyPrint();
-}
+  allocationResponse.parentElement.classList.remove('prettyprinted');
+  allocationResponse.append(JSON.stringify(data, null, 2));
+  PR.prettyPrint();
+};
 
-const setMapAllocation = async target => {
-    const endpoint = 'https://d606brmdeh.execute-api.eu-west-2.amazonaws.com/dev/seat/allocation/234324'
-    target.innerHTML="Allocating...";
-    const response = await fetch(endpoint, {
-        method: 'get',
-        mode: 'cors'
-    });
-    const data = await response.json();
-    console.log(data)
+const setMapAllocation = async (target) => {
+  const endpoint =
+    'https://d606brmdeh.execute-api.eu-west-2.amazonaws.com/dev/seat/allocation/234324';
+  target.innerHTML = 'Allocating...';
+  const response = await fetch(endpoint, {
+    method: 'get',
+    mode: 'cors',
+  });
+  const data = await response.json();
+  console.log(data);
 
-    target.innerHTML="Allocate!";
-    const allocationResponse = document.getElementById('setMapAllocation');
+  target.innerHTML = 'Allocate!';
+  const allocationResponse = document.getElementById('setMapAllocation');
 
-    allocationResponse.parentElement.classList.remove('prettyprinted');
-    allocationResponse.append(JSON.stringify(data, null, 2));
-    PR.prettyPrint();
-}
+  allocationResponse.parentElement.classList.remove('prettyprinted');
+  allocationResponse.append(JSON.stringify(data, null, 2));
+  PR.prettyPrint();
+};
